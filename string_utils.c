0ae7a0cafecc3115805197a30426bf8d9a36f782
endritNovaku <ennald@outlook.com>
2022-04-07 02:02:17 -0700

    _strcmp

--- string_utils.c
+++ string_utils.c
@@ -74,3 +74,27 @@ char *_strdup(char *string)
 		copy[i] = string[i];
 	return (copy);
 }
+/**
+ * _strcmp - compares two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 1 -1 or 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i, diff;
+
+	i = 0;
+	while (s1[i] != '\0' || s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			diff = s1[i] - s2[i];
+			break;
+		}
+		else
+			diff = 0;
+		i++;
+	}
+	return (diff);
+}
