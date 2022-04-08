f38a3723003f53f143e2eb3dd81c93075abd80cd
endritNovaku <ennald@outlook.com>
2022-04-08 01:37:20 -0700

    fixing betty

--- shell.h
+++ shell.h
@@ -9,13 +9,14 @@
 #include <sys/stat.h>
 /* Global variable for the environment */
 extern char **environ;
-/* String utilites */
+/* String utilities */
 int _strlen(char *string);
 char *_strcat(char *dest, char *src);
 char *_strcpy(char *dest, char *src);
 char *_strdup(char *string);
 int _strcmp(char *s1, char *s2);
 int _str_n_cmp(char *s1, char *s2, int n);
+/* Environment utilities */
 int _printenv(void);
 char *_getenv(char *name);
 #endif
